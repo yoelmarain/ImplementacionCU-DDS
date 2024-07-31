754152454beba0b95146109bf416142fb117a665
universoparalelo <77177224+universoparalelo@users.noreply.github.com>
2024-07-31 11:11:46 -0300

    Añado un proyecto de django para consultar tarifas

---
+++ Nueva carpeta/tarifas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Tarifa
+from .models import Vehiculo
+
+# Register your models here.
+admin.site.register(Tarifa)
+admin.site.register(Vehiculo)
+

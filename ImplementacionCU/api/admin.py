1a65d79825b7b55794c5edea8ef1633d81374248
yoelmarain <134217892+yoelmarain@users.noreply.github.com>
2024-08-29 10:51:56 -0300

    Delete unused files and directories

--- ImplementacionCU/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import Vehiculo, tarifas
-
-admin.site.register(Vehiculo)
-admin.site.register(tarifas)
